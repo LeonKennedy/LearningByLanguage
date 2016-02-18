c08a41acaddc5727e4f1c36957876f303aec5533
coffee <lionhe0119@hotmail.com>
2016-02-18 22:29:13 +0800

    new thread

--- c/thread.c
+++ c/thread.c
@@ -14,3 +14,46 @@ pthread_key_t key;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 unsigned long abc = 0;
+
+void *Test03(void *p)
+{
+    printf("Cancel point");
+    return NULL;
+}
+
+void *Test01(void *ptr)
+{
+    pthread_cleanup_push(Test03,NULL);
+    while(1){
+        abc++;
+        pthread_testcancel(); 
+    }
+    pthread_cleanup_pop(0);
+    return NULL;
+}
+
+void *Test02(void *ptr)
+{
+    while(1){
+        sleep(2);
+        printf("222cond_wait:abc =0x%08x\n",abc); 
+    }
+    return NULL;
+}
+
+int main(void)
+{
+    pthread_t tid1,tid2;
+    int ret;
+    
+    printf("Start:\n");
+    ret = pthread_create(&tid1, NULL, Test01, NULL);
+    ret = pthread_create(&tid2, NULL, Test02, NULL);
+    
+    sleep(6);
+    pthread_cancel(tid1);
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+
+    return 0;
+}
