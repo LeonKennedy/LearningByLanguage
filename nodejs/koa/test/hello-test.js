d1ae3cb023dd21d03fcd5036b49fcb03646361fb
LeonKennedy <lionhe0119@hotmail.com>
2017-08-15 14:37:32 +0800

    sytle(test)

--- nodejs/koa/test/hello-test.js
+++ nodejs/koa/test/hello-test.js
@@ -1,9 +1,31 @@
 const assert =  require('assert');
 
 describe('#hello.js', () => {
+    before(() => {
+        console.log('hello, before');
+    });
     describe('iner', () => {
+        before(function () {
+            console.log('before.');
+        });
+        
+        after(function () {
+            console.log('after.');
+        });
+        
         it('1-', () => {
             assert.strictEqual(0, 0);
+        });
+        beforeEach(function () {
+            console.log('iner.beforeEach');
+        });
+        afterEach(function () {
+            console.log('iner.afterEach');
+        });
+    });
+    describe('ouer', () => {
+        it('-2-', () => {
+            assert.strictEqual(1, 1);
         });
     });
 });
