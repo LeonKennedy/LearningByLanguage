eb8dd2c02ef052afacacde16e637b9be01375493
leonkennedy <lionhe0119@hotmail.com>
2017-11-01 16:04:37 +0800

    add MultiLabelBinarizer

--- python/ipython/README.md
+++ python/ipython/README.md
@@ -3,4 +3,4 @@
 http://htmlpreview.github.io/?
 + 01 sklean digits dataset; svc作分类 识别数字
 + 02 iris dataset鸢尾花数据集 svc分类
-+ 03 prerocessing 预处理 LabelBinarizer数值特征二值化
++ 03 prerocessing 预处理 LabelBinarizer数值特征二值化 LabelEncoder 字符串转化 MultiLabelBinarizer多标签二值化
--- python/ipython/ipython-3.py
+++ python/ipython/ipython-3.py
@@ -11,3 +11,30 @@ print(lb.transform([1,3,4]))
  [0 0 1 0 0]
   [0 0 0 1 0]]
 '''
+
+#help(preprocessing.LabelBinarizer)
+
+mlb = preprocessing.MultiLabelBinarizer()
+print(mlb.fit_transform([(1, 2), (3,)]))
+#[[1 1 0], [0 0 1]]
+print(mlb.classes_)
+# [1 2 3]
+
+
+le = preprocessing.LabelEncoder()
+le.fit([1, 2, 2, 6])
+le.classes_
+#[1, 2, 6]
+le.transform([1, 1, 2, 6])
+# [0, 0, 1, 2]
+le.inverse_transform([0, 0, 1, 2])
+# [1, 1, 2, 6]
+
+le = preprocessing.LabelEncoder()
+le.fit(["paris", "paris", "tokyo", "amsterdam"])
+list(le.classes_)
+# ['amsterdam', 'paris', 'tokyo']
+le.transform(["tokyo", "tokyo", "paris"])
+# [2, 2, 1]
+list(le.inverse_transform([2, 2, 1]))
+# ['tokyo', 'tokyo', 'paris']
