b554e2ff3c36bde9d671224596ce1eccdba4ae16
leonkennedy <lionhe0119@hotmail.com>
2018-10-16 14:17:47 +0800

    fix closure

--- python/closure.py
+++ python/closure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 __author__ = "olenji"
 
+
 class A:
     def here(self):
         data = "A.here"
@@ -16,6 +17,26 @@ class B:
         data = "B.here"
         callbackfuc(data)
 
+
+# -------------------
+
+def make_average():
+  series = []
+  def avg(p):
+    series.append(p)
+    return sum(series) / len(series)
+  return avg
+    
+    
 if __name__ == '__main__':
     aa = A()
     aa.here()
+    print('---- other ----')
+    avg = make_average()
+    avg(10)
+    print(avg(11))
+    print(avg(12))
+    print(avg.__code__.co_varnames)
+    print(avg.__code__.co_freevars)
+    print("avg.__closure__ \t%s" % avg.__closure__)
+    print("avg.__closure__[0].cell_contents: \t%s" % avg.__closure__[0].cell_contents)
