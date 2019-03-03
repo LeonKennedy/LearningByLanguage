333c50730e724448a28ba56c7cb507972efa18b7
LeonKennedy <lionhe0119@hotmail.com>
2019-03-04 00:57:14 +0800

    fix(myasycio)

--- python/myasyncio.py
+++ python/myasyncio.py
@@ -9,10 +9,32 @@
 
 
 import asyncio
+import time, datetime
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
 
 async def main():
-    print('hello')
-    await asyncop.sleep(1)
-    print('world')
+    print(f"started at {time.strftime('%X')}")
 
-asyncio.run(main())
+    res = await say_after(1, 'hello')
+    print(res)
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+# ----------
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if (loop.time() + 1.0) >= end_time:
+            break
+        await asyncio.sleep(1)
+
+#asyncio.run(display_date())
+# ------------
