28524b9169a2945324e63ff45c88b14bd7727cbb
LeonKennedy <lionhe0119@hotmail.com>
2020-07-18 11:53:13 +0800

    feat(c) learning cmake

---
+++ c/tutorial/CmakeConfig.h
@@ -0,0 +1,4 @@
+#define S_CMAKE_MODULE_PATH "/Users/coffee/Documents/code/LearningByLanguage/c/tutorial/cmake"
+#define S_CMAKE_CXX_STANDARD 11
+#define S_CMAKE_CXX_STANDARD_REQUIRED ON
+#define S_CMAKE_CXX_FLAGS_DEBUG -g -Wall
