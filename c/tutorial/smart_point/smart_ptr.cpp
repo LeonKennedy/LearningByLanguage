0356375efb32b320e7936cc847d2959064ba8f34
LeonKennedy <lionhe0119@hotmail.com>
2020-08-23 19:18:02 +0800

    feat(c) smart & dynamic

--- c/tutorial/smart_point/smart_ptr.cpp
+++ c/tutorial/smart_point/smart_ptr.cpp
@@ -1,19 +1,63 @@
 #include "smart_ptr.hpp"
 #include <iostream>
 
+
 template <typename T>
-SmartPtr<T>::SmartPtr(SmartPtr&& other)
+template <typename U>
+SmartPtr<T>::SmartPtr(SmartPtr<U>& other)
 {
-    ptr_ = other.release();
+    ptr_ = other.ptr_;
+    if (ptr_) {
+        other.shared_cout_->add();
+        shared_cout_ = other.shared_cout_;
+    }
 }
 
 template <typename T>
-SmartPtr<T>& SmartPtr<T>::operator=(SmartPtr<T> other)
+SmartPtr<T>::SmartPtr(SmartPtr&& other)
+{
+    ptr_ = other.ptr_;
+    if (ptr_) {
+        shared_cout_ = other.shared_cout_;
+        other.ptr_ = nullptr;
+    }
+}
+
+template <typename T>
+template <typename U>
+SmartPtr<T>::SmartPtr(const SmartPtr<U>& other, T* p)
+{
+    ptr_ = p;
+    if (ptr_) {
+        other.shared_cout_->add();
+        shared_cout_ = other.shared_cout_;
+    }
+}
+
+template <typename T>
+SmartPtr<T>& SmartPtr<T>::operator=(SmartPtr other)
 {
     other.swap(*this);
     return *this;
 }
 
+template <typename T>
+SmartPtr<T>::~SmartPtr()
+{
+    if (ptr_ && !shared_cout_->sub()) {
+        delete ptr_;
+        delete shared_cout_;
+    }
+}
+
+template <typename T>
+long SmartPtr<T>::use_count() const
+{
+    if (ptr_) 
+        return shared_cout_->count();
+    else
+        return 0;
+}
 
 template <typename T>
 T * SmartPtr<T>::release()
@@ -27,14 +71,34 @@ template <typename T>
 void SmartPtr<T>::swap(SmartPtr & s)
 {
     std::swap(ptr_, s.ptr_);
+    std::swap(shared_cout_, s.shared_cout_);
+}
+
+// template <typename T>
+// std::ostream & operator<< (std::ostream & os, SmartPtr<T> & p)
+// {
+//     os << p.ptr_->count() << "\tshard_count:" << p.shared_cout_->count() << std::endl; 
+// }
+
+template <typename T, typename U>
+SmartPtr<T> dynamic_pointer_cast(const SmartPtr<U>& other)
+{
+    T * ptr = dynamic_cast<T*>(other.get());
+    return SmartPtr<T>(other, ptr);
 }
 
 int main(int argc, char const *argv[])
 {
     Box * b = new Box(5);
     SmartPtr<Box> p(b);
-    SmartPtr<Box> p2 {std::move(p)};
+    printf("use cout of ptr1 is %ld\n", p.use_count());
+    SmartPtr<shape> p2(p);
+    printf("use cout of ptr2 is %ld\n", p2.use_count());
+    SmartPtr<Box> p3 {std::move(p)};
+    printf("use cout of ptr3 is %ld\n", p3.use_count());
+    SmartPtr<Box> p4 = dynamic_pointer_cast<Box>(p3);
+    printf("use cout of ptr3 is %ld\n", p4.use_count());
     // p2 = std::move(p);
-    std::cout << p2.get()->count() << std::endl;
+    std::cout << p2.get() << std::endl;
     return 0;
 }
--- c/tutorial/smart_point/smart_ptr.hpp
+++ c/tutorial/smart_point/smart_ptr.hpp
@@ -1,29 +1,65 @@
+#include <iostream>
+
+class UseCount
+{
+    private:
+        long cnt_;
+    public:
+        UseCount(long c=1): cnt_(c) {}
+        void add() { cnt_++; }
+        long sub() { return --cnt_ ;}
+        long count() const { return cnt_;}
+};
 
 template <typename T>
 class SmartPtr
 {
     private:
         T * ptr_;
+        UseCount * shared_cout_;
     public:
+        template <typename U>
+        friend class SmartPtr;
         explicit SmartPtr(T * p=nullptr)
-            : ptr_(p) {}
+            : ptr_(p) 
+        {
+            if (p) {
+                shared_cout_ = new UseCount;
+            }
+        }
+        template <typename U>
+        SmartPtr(SmartPtr<U>&);
         SmartPtr(SmartPtr&&);
-        ~SmartPtr() { delete ptr_; };
+        template <typename U>
+        SmartPtr(const SmartPtr<U>&, T*);
+        ~SmartPtr();
 
         SmartPtr& operator=(SmartPtr);
         T& operator*() const { return *ptr_;}
         T* operator->() const { return ptr_;}
         operator bool() const { return ptr_;}
         T * get() const { return ptr_; };
+        long use_count() const;
         T * release();
         void swap(SmartPtr&);
+
+        // friend std::ostream & operator<< (std::ostream &, SmartPtr<T> &);
 };
 
-class Box
+class shape
+{
+    public:
+        virtual ~shape() {}
+};
+
+class Box: public shape
 {
     private:
         int size_;
     public:
         Box(int size = 0): size_(size) {};
         int& count() {return size_;}
+        ~Box() { puts("~Box()");}
 };
+
+
