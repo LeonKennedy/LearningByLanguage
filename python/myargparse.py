303dd6c608f8c3b11c68b70b61194d008a09435e
LeonKennedy <lionhe0119@hotmail.com>
2021-04-02 17:20:34 +0800

    feat(python) argspare useage

--- python/myargparse.py
+++ python/myargparse.py
@@ -1,39 +1,83 @@
 #!/usr/bin/python
-#encoding=utf-8
+# encoding=utf-8
 '''
- author     : olenji
+ author     : coffee
  date       : 2016-12-14 14:20:00
- function   : 图片操作
- py_version ：2.7.2
+ function   : argsparse
+ py_version ：3.8
 '''
 
-from PIL import Image
-
-import argparse  
-  
-#### 参数调用说明  
-parser = argparse.ArgumentParser()  
-parser.add_argument('-t','--tcp', help='tcp service',action='store_true')  
-parser.add_argument('-u', '--udp', help='udp service', action='store_true')  
-parser.add_argument('-s', '--scheduler', help='one of rr|wrr|lc|wlc|lblc|lblcr|dh|sh|sed|nq,the default scheduler is wlc')  
-parser.add_argument('-p', '--persistent', help='persistent service, default:1500', type = int)  
-parser.add_argument('-r', '--realserver', help="server-address is host (and port) Example: -r '1.1.1.1 2.2.2.2 3.3.3.3'", type = str )  
-parser.add_argument('-f', '--floapingip', help='vip address of lvs' )  
-parser.add_argument('-g', '--gatewaying', help='gatewaying (direct routing)',action='store_true')  
-parser.add_argument('-m','--masquerading', help='masquerading (NAT)', action='store_true')  
-parser.add_argument('-S','--srcport', help='listen on floapip port', type = int )  
-parser.add_argument('-R','--destport', help='listen on realserver port', type = int )  
-args = parser.parse_args()  
-
-#action='store_true' 表明该参数不接受参数传递， 即， 只接受 -t 或 --tcp 下面是错误例子 -t tcp  或 --tcp=yes  
-#假如需要接受参数， 如 -t tcp 或 --tcp=yes 那么不可以使用 action 进行定义  
-#假如需定义参数传入格式， 可使用 type = int|str|complax 进行定义  
+import argparse
 
 
-#假如执行过程中， 调用参数 -S 80 或 --srcport=80  传递  
-#args.srcport = 80 
+def init_args():
+    parser = argparse.ArgumentParser()
 
-#执行时可能存在多个空格  
-#/app.py --realserver="1.1.1.1 2.2.2.2   3.3.3.3       4.4.4.4"  
-#realip =  re.sub(r'\s+', ' ',args.realserver)  
-#realip 格式变成 1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4 只存在一个空格分隔符
+    parser.add_argument('-s', '--scheduler',
+                        help='one of rr|wrr|lc|wlc|lblc|lblcr|dh|sh|sed|nq,the default scheduler is wlc')
+    parser.add_argument('-p', '--persistent', help='persistent service, default:1500', type=int)
+    parser.add_argument('-r', '--realserver',
+                        help="server-address is host (and port) Example: -r '1.1.1.1 2.2.2.2 3.3.3.3'", type=str)
+    parser.add_argument('-f', '--floapingip', help='vip address of lvs')
+    parser.add_argument('-g', '--gatewaying', help='gatewaying (direct routing)', action='store_true')
+    parser.add_argument('-m', '--masquerading', help='masquerading (NAT)', action='store_true')
+    parser.add_argument('-S', '--srcport', help='listen on floapip port', type=int)
+    parser.add_argument('-R', '--destport', help='listen on realserver port', type=int)
+
+    return args
+
+
+# 互斥
+def exp_mutually(parser):
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-t', '--tcp', help='tcp service', action='store_true')  # default false
+    group.add_argument('-u', '--udp', help='udp service', action='store_true')
+    args = parser.parse_args()
+    print("tcp: ", args.tcp, "udp:", args.udp)
+
+
+def exp1(parser):
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+# MetavarTypeHelpFormatter 使用类型作为metavar
+def exp2(parser):
+    parser = argparse.ArgumentParser(
+        prog='PROG',
+        formatter_class=argparse.MetavarTypeHelpFormatter)
+    parser.add_argument('--foo', type=int, required=True)
+    parser.add_argument('bar', type=float)
+    args = parser.parse_args()
+    print(args.bar, args.foo)
+
+
+def exp3(parser):
+    parser.add_argument("--foo", action="extend", nargs="+", type=str)
+    print(parser.parse_args(["--foo", "f1", "--foo", "f2", "f3", "f4"]))
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    print(parser.parse_args(['-vvv']))
+
+def exp4(parser):
+    parser.add_argument('--foo', nargs='?', const='c', default='d')
+    parser.add_argument('bar', nargs='?', default='d')
+    print(parser.parse_args(['XX', '--foo', 'YY']))
+    print(parser.parse_args(['XX', '--foo']))
+    print(parser.parse_args([]))
+
+# 假如需定义参数传入格式， 可使用 type = int|str|complax 进行定义
+
+
+# 执行时可能存在多个空格
+# /app.py --realserver="1.1.1.1 2.2.2.2   3.3.3.3   4.4.4.4"
+# realip =  re.sub(r'\s+', ' ',args.realserver)
+# realip 格式变成 1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4 只存在一个空格分隔符
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True)
+    exp4(parser)
